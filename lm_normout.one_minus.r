cf5f254ec1bf10fe58e31501628eda18ad35a46d
gbonett <greg.bonett@gmail.com>
2013-07-17 22:09:56 -0700

    added 'outcome ~ assignment` model

--- lm_normout.one_minus.r
+++ lm_normout.one_minus.r
@@ -1,9 +1,10 @@
 # Covar Selection with treatment effect interaction
 N<-30
-size<-10000
+size<-20000
 delta<-1
 mat3<-c()
 mat2<-c()
+mat1<-c()
 
 for(i in 1:size){
 
@@ -16,8 +17,10 @@ for(i in 1:size){
   #generate linear model
   ss3_lm <- lm(outcome ~ assignment + priority + assignment*priority)
   ss2_lm <- lm(outcome ~ assignment + priority)
+  ss1_lm <- lm(outcome ~ assignment)
   mat3<-rbind(mat3,summary(ss3_lm)$coef[,1]/summary(ss3_lm)$coef[,2])
   mat2<-rbind(mat2,summary(ss2_lm)$coef[,1]/summary(ss2_lm)$coef[,2])
+  mat1<-rbind(mat1,summary(ss1_lm)$coef[,1]/summary(ss1_lm)$coef[,2])
 
 }
 
@@ -31,15 +34,18 @@ names(coef(lm))
  mean(mat2[,2])
  mean(mat2[,3])
 
+ mean(mat1[,1])
+ mean(mat1[,2])
 
 
 
 #Random Selection with treatment effect interaction
 N<-30
-size<-10000
+size<-20000
 delta<-1
 mat3<-c()
 mat2<-c()
+mat1<-c()
 
 for(i in 1:size){
 
@@ -54,8 +60,10 @@ for(i in 1:size){
   #generate linear model
   ss3_lm <- lm(outcome ~ assignment + priority + assignment*priority)
   ss2_lm <- lm(outcome ~ assignment + priority)
+  ss1_lm <- lm(outcome ~ assignment)
   mat3<-rbind(mat3,summary(ss3_lm)$coef[,1]/summary(ss3_lm)$coef[,2])
   mat2<-rbind(mat2,summary(ss2_lm)$coef[,1]/summary(ss2_lm)$coef[,2])
+  mat1<-rbind(mat1,summary(ss1_lm)$coef[,1]/summary(ss1_lm)$coef[,2])
 
 }
 
@@ -69,3 +77,5 @@ mean(mat2[,1])
 mean(mat2[,2])
 mean(mat2[,3])
 
+mean(mat1[,1])
+mean(mat1[,2])
